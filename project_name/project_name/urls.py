f367886857bfcfd87512c19f6b8f2fda2d536349
Tom Henderson <tomhenderson@mac.com>
2014-12-22 10:51:10 +1300

    Tweaked project layout and added login / logout views.

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -9,20 +9,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
-
-    # Examples:
-    # url(r'^$', '{{ project_name }}.views.home', name='home'),
-    # url(r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', views.log_out, name='log_out'),
     url(r'^admin/', include(admin.site.urls)),
 )
-
-# Uncomment the next line to serve media files in dev.
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
