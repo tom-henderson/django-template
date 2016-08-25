2336a72c878a58ef8a3a1f47f6ecf9ee9574feca
Tom Henderson <tomhenderson@mac.com>
2016-08-25 18:52:47 +1200

    Update url pattern syntax for django 1.10

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.contrib import admin
@@ -8,12 +8,12 @@ import views
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='{{project_name}}/base.html')),
     url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', views.log_out, name='log_out'),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
